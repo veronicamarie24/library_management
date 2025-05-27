db9a5d6a2376ed00ce98e2e42b3677057cb66822
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2025-05-27 13:44:40 -0700

    adjust login css

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,9 +4,10 @@ export default function Login() {
   return (
     <div className="login-container">
       <h1>Login</h1>
+      Choose your role
       <div className="login-buttons">
-        <button>Login as Admin</button>
-        <button>Login as User</button>
+        <button className="button-secondary">Login as Admin</button>
+        <button className="button-primary">Login as User</button>
       </div>
     </div>
   );
